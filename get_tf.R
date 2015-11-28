d2ad793798330819818732fada367214f5665403
Bailey <baileypiggott@gmail.com>
2015-11-28 16:00:15 -0500

    function to create transfer function

---
+++ get_tf.R
@@ -0,0 +1,30 @@
+
+
+get_tf <- function(data_in, data_out, freq){
+   
+  tf <- vector(mode = 'numeric', length = length(freq))
+  
+  F_in <- matrix(ncol = length(freq), nrow = ncol(data_in))
+  F_out <- matrix(ncol = length(freq), nrow = ncol(data_out))
+  
+  for(j in 1:length(freq)){
+    
+    F_in[,j] <- t(data_in[freq[j],]) #take row corresponding to jth chosen frequency
+    F_out[,j] <- t(data_out[freq[j],]) #take row corresponding to jth chosen frequency
+    
+  }
+  
+  for(i in 1:length(freq)){
+    tf[i] <- ginv(F_in[,i]) %*% F_out[,i]
+  }
+  
+  # create data frames of tf magnitude and phase ----------------
+  p=1/2*length(tf)
+  mag <- abs(tf)
+  phase <- atan2(Im(tf), Re(tf))
+  
+  tf <- data.frame(freq = seq(0,0.5,0.5/p), mag = mag[1:(p+1)], phase = phase[1:(p+1)])
+  tf <- tf %>% gather(type, val, mag:phase)
+  
+  return(tf)
+}
